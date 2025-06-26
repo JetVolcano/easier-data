41899ab02020cbdc9fa650d26ee1dd4f014d7d0a
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-06-26 10:20:17 -0700

    Refactor __init__.py and arrays.py: Update docstrings and improve __all__ declaration

--- easier_data/__init__.py
+++ easier_data/__init__.py
@@ -1,4 +1,8 @@
+"""
+# Easier Data
+"""
+
 from .arrays import Array1D, Array2D, Array3D
 
 
-__all__: list[str] = ["arrays.py", Array1D, Array2D, Array3D]
+__all__: list[str] = [Array1D, Array2D, Array3D]
